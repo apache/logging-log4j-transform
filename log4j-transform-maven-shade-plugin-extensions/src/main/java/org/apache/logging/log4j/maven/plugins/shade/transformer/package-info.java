f0c86808cca9b178e2e2a90402fece1e87914bf8
Volkan Yazıcı <volkan@yazi.ci>
2023-10-20 16:34:20 +0200

    Implement API checks using `bnd-baseline:baseline`

--- log4j-transform-maven-shade-plugin-extensions/src/main/java/org/apache/logging/log4j/maven/plugins/shade/transformer/package-info.java
+++ log4j-transform-maven-shade-plugin-extensions/src/main/java/org/apache/logging/log4j/maven/plugins/shade/transformer/package-info.java
@@ -14,9 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@Export
 @Version("0.1.0")
-package org.apache.logging.log4j.weaver;
+package org.apache.logging.log4j.maven.plugins.shade.transformer;
 
-import org.osgi.annotation.bundle.Export;
 import org.osgi.annotation.versioning.Version;
